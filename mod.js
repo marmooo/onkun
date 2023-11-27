c78d36c379c7a140c3a5e6ef4abb7f3765788162
marmooo <marmooo@users.noreply.github.com>
2023-11-27 22:17:30 +0900

    fix

--- mod.js
+++ mod.js
@@ -21,14 +21,14 @@ export class Onkun {
     });
   }
 
-  async fetchUnihan(url, options) {
+  async fetch(name, url, options) {
     const response = await fetch(url, options);
     const text = await response.text();
     text.trimEnd().split("\n").forEach((line) => {
       const [kanji, onkun] = line.split(",");
       if (kanji in this.dict === false) this.dict[kanji] = {};
       const info = this.dict[kanji];
-      info["Unihan"] = onkun.split(" ");
+      info[name] = onkun.split(" ");
     });
   }
 
