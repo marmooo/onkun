72b27ceb8c9181b2e220199dca55e50a1bd4e835
marmooo <marmooo@users.noreply.github.com>
2025-01-08 01:14:42 +0900

    improve test

--- test.js
+++ test.js
@@ -12,6 +12,16 @@ Deno.test("Simple check", async () => {
   assertEquals(onkun.get("漢")["Unihan"], ["カン", "タン", "から"]);
 });
 
+Deno.test("Simple check", async () => {
+  const onkun = new Onkun();
+  await onkun.loadJoyo("data/joyo-2017.csv");
+  await onkun.load("Unihan", "data/Unihan-2024-07-31.csv");
+  assertEquals(onkun.get("漢")["小学"], ["カン"]);
+  assertEquals(onkun.get("漢")["中学"], []);
+  assertEquals(onkun.get("漢")["高校"], []);
+  assertEquals(onkun.get("漢")["Unihan"], ["カン", "タン", "から"]);
+});
+
 Deno.test("2010/2017 check", async () => {
   const onkun = new Onkun();
   await onkun.loadJoyo("data/joyo-2017.csv");
